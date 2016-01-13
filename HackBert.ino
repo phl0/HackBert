37cb221a783576582a92e90c2c53db8d22a1f94d
phl0 <florian@florian-wolters.de>
2016-01-13 13:50:20 +0100

    Corrected DEBUG option; should be commented out if not needed instead of setting it to 0 because that would also count as DEBUG beeing defined

--- HackBert.ino
+++ HackBert.ino
@@ -13,7 +13,7 @@ Adafruit_VS1053_FilePlayer musicPlayer =
 #define para_playSpeed 0x1E04
 
 // Enable or disable debugging via serial monitor here
-#define DEBUG        0
+//#define DEBUG
 
 // constants won't change
 
